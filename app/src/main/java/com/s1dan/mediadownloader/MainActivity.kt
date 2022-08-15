05a0e2cd55c7ddf3856071f0b1595e8cfb834022
s1Dan <dannikg@mail.ru>
2022-08-15 14:39:35 +0300

    Initial commit

---
+++ app/src/main/java/com/s1dan/mediadownloader/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.s1dan.mediadownloader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
